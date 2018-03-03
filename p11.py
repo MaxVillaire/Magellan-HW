867bf6e18d6520d74af984fd489db5e13051d8bb
Max Villaire <villaire@email.sc.edu>
2018-03-03 15:44:52 -0500

    updated problem 11

--- p11.py
+++ p11.py
@@ -8,9 +8,8 @@ a = np.arange(93)
 b = np.ones(a.size)
 
 print(a)
-
-print(b)
-
+print(a.T)
+print(b.T)
 
 #2)
 print('Problem 2) Create 3x3 Array of \'True\'s')
@@ -19,9 +18,31 @@ c = np.full((3,3), True)
 print(c)
 
 #3)
-print('Problem 3) Replpace odds in a with -1')
-d = a
-d[1::2] = -1
+print('Problem 3) Replpace odds in \'a\' with -1')
 a[1::2] = -1
 
-print(d)
+print(a)
+
+#4)
+print('Problem 4) Reshape \'a\' to a 2d array with 3 rows')
+a = np.reshape(a,(3,-1))
+
+print(a)
+print(a.T)
+
+##5)
+#print('Problem 5) Stack \'a\' and \'b\' vertically')
+#print(np.stack((a,b), axis=-1))
+#
+##6)
+#print('Problem 6) Transpose a & b, then stack them horizontally')
+#print(np.hstack((a.T,b.T)))
+
+#7)
+print('Problem 7) Create c=np.array([1,2,3]) using only nunmpy functions')
+c = np.arange(1,4)
+
+print(c)
+print(type(c))
+
+
